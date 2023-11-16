522b8a7c00448ae3de1d4e044573adf88dabc6f7
Шабанов Сергей <shabanov_s@TOP.ACADEMY>
2023-11-16 21:27:43 +0300

    Не работает удаление робота из массива

--- src/index.js
+++ src/index.js
@@ -1,2 +1,65 @@
 import './style.css';
 import './main.scss';
+
+class Robot {
+    constructor(id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+}
+
+let robots = [
+    new Robot(1, "First", "first@robot.ru"),
+    new Robot(2, "Second", "second@robot.ru"),
+    new Robot(3, "Third", "third@robot.ru"),
+    new Robot(4, "First", "first@robot.ru"),
+    new Robot(5, "Second", "second@robot.ru"),
+    new Robot(6, "Third", "third@robot.ru"),
+];
+
+function appendRobot(robot) {
+    const robotContainer = document.createElement("div");
+    robotContainer.classList.add("robot");
+    robotContainer.dataset.id = robot.id;
+    robotContainer.dataset.name = robot.name;
+    robotContainer.dataset.email = robot.email;
+
+    const robotAvatar = document.createElement("img");
+    robotAvatar.classList.add("robot__avatar");
+    robotAvatar.src = `https://robohash.org/${robot.id}?size=200x200`;
+    robotContainer.append(robotAvatar);
+
+    const robotName = document.createElement("h3");
+    robotName.innerHTML = robot.name;
+    robotContainer.append(robotName);
+
+    const robotEmail = document.createElement("p");
+    robotEmail.innerHTML = robot.email;
+    robotContainer.append(robotEmail);
+
+    const removeButton = document.createElement("button");
+    removeButton.innerHTML = "Remove";
+    removeButton.onclick = (e) => {
+        robotContainer.remove();
+        console.log(robots) // Должно удалять элемент из массива
+    }
+    robotContainer.append(removeButton);
+
+    document.querySelector(".robots").append(robotContainer);
+}
+
+robots.forEach((robot) => appendRobot(robot));
+
+function createRobot(e) {
+    let robotID = Math.ceil(Math.random() * 10);
+    let robotName = document.querySelector("#name").value;
+    let robotEmail = document.querySelector("#email").value;
+
+    appendRobot(new Robot(robotID, robotName, robotEmail));
+
+    document.querySelector("#name").value = "";
+    document.querySelector("#email").value = "";
+}
+
+document.querySelector("#create").onclick = createRobot;
